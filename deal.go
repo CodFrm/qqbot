93954cbeaf64942947ce59c61a972022efeb2f4f
CodFrm <code.farmer@qq.com>
2020-09-24 12:01:01 +0800

    修改文案

--- deal.go
+++ deal.go
@@ -82,7 +82,7 @@ func dealUniversal(args iotqq.Message) bool {
 		return true
 	} else if _, ok := args.CommandMatch("帮助"); ok {
 		args.SendMessage("1.优惠购物,触发指令:'有无[物品名]',可获取商品列表和内部优惠券,选择你心爱的物品下单吧\n" +
-			"2.口令转换,触发指令:'[口令或者链接]',可查询优惠券和优惠口令\n" +
+			"2.优惠查券,触发指令:'[口令或者链接]',可查询优惠券和优惠口令\n" +
 			"3.订阅商品,触发指令:'订阅[关键字]',订阅指定的商品,如果有活动会直接私聊推送给你哦\n" +
 			"饿了么每日红包:$5YiUccAeTlY$")
 		return true
